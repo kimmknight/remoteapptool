1e6da1656039888ee3e6b368ecfe3750a6a333b5
Kim <kimknight@gmail.com>
2021-01-22 22:02:59 +1100

    Update version number for version 6.0.0.0 release

--- remoteapp-tool/My Project/AssemblyInfo.vb
+++ remoteapp-tool/My Project/AssemblyInfo.vb
@@ -11,8 +11,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("RemoteApp Tool")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("Kim Knight")> 
-<Assembly: AssemblyProduct("RemoteApp Tool")> 
-<Assembly: AssemblyCopyright("Created by Kim Knight")> 
+<Assembly: AssemblyProduct("RemoteApp Tool")>
+<Assembly: AssemblyCopyright("")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.4.0.18")>
-<Assembly: AssemblyFileVersion("5.4.0.18")>
+<Assembly: AssemblyVersion("6.0.0.0")>
+<Assembly: AssemblyFileVersion("6.0.0.0")>
